e6bcc93035a413efbba08d30e4e36d0466cb2a2c
NoelNim <noel@nimstad.com>
2021-03-17 19:25:56 +0100

    map fullscreen

---
+++ otherPages/maps/map.js
@@ -0,0 +1,6 @@
+mapboxgl.accessToken = 'pk.eyJ1Ijoibmltc3RhZCIsImEiOiJja21kcjEzZW8ybzE0MnFrbjVhZWN4aDc3In0.a_w7b05avPXP0Jl6v2luAw';
+var map = new mapboxgl.Map
+({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11'
+});
